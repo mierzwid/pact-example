e75722a2253320546e9e717b4665c0af63bd88d0
mierzwid <mierzwid@gmail.com>
2020-11-05 11:17:55 +0100

    fluency

--- src/test/kotlin/org/mierzwid/pact/demo/RateServiceIntegrationTest.kt
+++ src/test/kotlin/org/mierzwid/pact/demo/RateServiceIntegrationTest.kt
@@ -1,26 +1,25 @@
 package org.mierzwid.pact.demo
 
+import org.junit.Assert.assertEquals
+import org.junit.Test
 import org.mierzwid.pact.demo.rates.Code
 import org.mierzwid.pact.demo.rates.Rate
 import org.mierzwid.pact.demo.rates.RateClient
 import org.mierzwid.pact.demo.rates.RateService
-import org.junit.Assert.assertEquals
-import org.junit.Test
 
 class RateServiceIntegrationTest {
 
     @Test
     fun shouldReturnUsdToEurRate() {
         // given
-        val client = object : RateClient {
+        val service = RateService(object : RateClient {
             override fun getRate(code: Code) =
                 when (code) {
                     Code.USD -> Rate.of(5.0)
                     Code.EUR -> Rate.of(4.0)
                     else -> throw Exception("It should never ask for different rates")
                 }
-        }
-        val service = RateService(client)
+        })
 
         // when
         val usdToEurRate = service.usdToEurRate()
