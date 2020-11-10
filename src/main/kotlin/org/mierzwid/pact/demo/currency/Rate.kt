9d35f9523989f6e34af5af0f8bf00b34c3a445ec
mierzwid <mierzwid@gmail.com>
2020-11-10 11:00:58 +0100

    introducing BigDecimal CR v2

--- src/main/kotlin/org/mierzwid/pact/demo/currency/Rate.kt
+++ src/main/kotlin/org/mierzwid/pact/demo/currency/Rate.kt
@@ -2,11 +2,13 @@ package org.mierzwid.pact.demo.currency
 
 import java.math.BigDecimal
 import java.math.MathContext
+import java.math.RoundingMode
 
 data class Rate(val value: BigDecimal) {
-    operator fun div(rate: Rate) = Rate(value.divide(rate.value, MathContext.UNLIMITED))
+    operator fun div(rate: Rate) = Rate(value.divide(rate.value, MATH_CONTEXT))
 
     companion object {
-        fun of(mid: Double) = Rate(mid.toBigDecimal(MathContext.UNLIMITED))
+        private val MATH_CONTEXT = MathContext(3, RoundingMode.CEILING)
+        fun of(mid: Double) = Rate(mid.toBigDecimal(MATH_CONTEXT))
     }
 }
